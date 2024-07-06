52ebff74b93779e78591144b3aa203a5de3bd4ad
Karver91 <o.kurmanov91@gmail.com>
2024-07-06 13:40:19 +0200

    Change in test files

--- test_login_page.py
+++ test_login_page.py
@@ -2,6 +2,7 @@ from pages.login_page import LoginPage
 
 
 def test_guest_should_see_login_page(browser):
-    login_page = LoginPage(browser)
+    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    login_page = LoginPage(browser, link)
     login_page.open()
     login_page.should_be_login_page()
