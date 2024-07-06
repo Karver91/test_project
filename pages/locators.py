72dd5f1838971f65bce4a1a02bc634bb09bdd785
Karver91 <o.kurmanov91@gmail.com>
2024-07-06 13:38:45 +0200

    Change in product page. Add new tests

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,19 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTRATION_FORM = (By.ID, "register_form")
+
+
+class ProductPageLocators:
+    ADD_PRODUCT = (By.XPATH, '//form[@id="add_to_basket_form"]/child::button[@type="submit"]')
+    ADD_PRODUCT_SUCCESS_ALERTS = (By.XPATH, '//div[@id="messages"]/descendant::div[@class="alertinner "]')
+    PRODUCT_NAME_ALERT = (By.XPATH, '//div[@id="messages"]/descendant::div[@class="alertinner "][1]/strong')
+    OFFER_ALERT = (By.XPATH, '//div[@id="messages"]/descendant::div[@class="alertinner "][2]/strong')
+    PRICE_ALERT = (By.XPATH, '//div[@id="messages"]/descendant::div[@class="alertinner "]/p/strong')
+    PRODUCT_NAME = (By.CSS_SELECTOR, 'div.product_main > h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, 'div.product_main > p.price_color')
+
---
+++ pages/product_page.py
@@ -0,0 +1,36 @@
+from time import sleep
+
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        self.should_be_add_product_to_basket_button()
+        self.add_product_is_success()
+        self.alerts_displayed_is_success()
+        self.alert_product_name_is_correct()
+        self.alert_product_price_is_correct()
+
+    def should_be_add_product_to_basket_button(self):
+        assert self.is_element_present(locator=ProductPageLocators.ADD_PRODUCT), \
+            f'Add_product button is not presented | locator {ProductPageLocators.ADD_PRODUCT}'
+
+    def add_product_is_success(self):
+        add_product_button = self.get_element(locator=ProductPageLocators.ADD_PRODUCT)
+        add_product_button.click()
+        self.solve_quiz_and_get_code()
+
+    def alerts_displayed_is_success(self):
+        alerts = self.get_multiple_elements(locator=ProductPageLocators.ADD_PRODUCT_SUCCESS_ALERTS)
+        assert len(alerts) == 3, "Alerts don't show"
+
+    def alert_product_name_is_correct(self):
+        product_name_alert = self.get_element_text(locator=ProductPageLocators.PRODUCT_NAME_ALERT)
+        product_name = self.get_element_text(locator=ProductPageLocators.PRODUCT_NAME)
+        assert product_name_alert == product_name, "Product name test is failed"
+
+    def alert_product_price_is_correct(self):
+        product_price_alert = self.get_element_text(locator=ProductPageLocators.PRICE_ALERT)
+        product_price = self.get_element_text(locator=ProductPageLocators.PRODUCT_PRICE)
+        assert product_price_alert == product_price, "Product price test is failed"
